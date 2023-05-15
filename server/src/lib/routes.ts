1992efe87927e64569793116b9803a4be221c2e3
Igor Andrade <rogiandradee@gmail.com>
2023-05-15 20:33:09 +0100

    start styles of home
    - start on authentication
    - start on home screen styles
    -someone bugs in routes.

--- server/src/lib/routes.ts
+++ server/src/lib/routes.ts
@@ -1,14 +1,53 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import jwt, { SignOptions } from 'jsonwebtoken';
 import { prisma } from "./prisma";
 import { z } from "zod";
+
+declare module "fastify" {
+  interface FastifyRequest {
+    user?: { userId: string };
+  }
+}
+
+interface RouteParams {
+  id: string
+}
 
 type UserData = {
   username: string;
   password: string;
 }
 
+export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
+
+  function getUserFromToken(authHeader: string) {
+    const token = authHeader.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return decoded;
+  }
+  
+  try {
+    const authHeader = request.headers.authorization;
+    if (!authHeader) {
+      throw new Error('Authorization header missing');
+    }
+
+    const user = await getUserFromToken(authHeader);
+
+    const decoded = jwt.verify(authHeader.split(' ')[1], process.env.JWT_SECRET as string) as { userId: string };
+    // Adicione o objeto decoded ao objeto de solicitação para que ele possa ser acessado em rotas subsequentes
+    request['user'] = decoded;
+    return;
+  } catch (error: any) {
+    console.error(error.message);
+    reply.status(401).send({ message: 'Unauthorized' });
+    throw new Error('Unauthorized');
+  }
+}
+
 export async function appRoutes(app: FastifyInstance) {
 
+  // Register routes
   app.get('/status', (req, res) => {
     res.status(200).send({ message: 'API is up and running' });
   });
@@ -56,8 +95,7 @@ export async function appRoutes(app: FastifyInstance) {
     reply.send({ message: "User created successfully" });
   });
 
-  app.get('/login', async (request, reply) => {
-
+  app.get('/login', { preHandler: authenticate }, async (request, reply) => {
     let userData: UserData;
 
     try {
@@ -84,6 +122,57 @@ export async function appRoutes(app: FastifyInstance) {
       return;
     }
 
-    reply.send({ message: "User logged in successfully" });
+    const jwtOptions: SignOptions = {
+      expiresIn: "1h",
+    };
+
+    // Se o usuário existir e a senha estiver correta, crie um token JWT e envie-o de volta para o cliente.
+    const token = jwt.sign(
+      { username: user.username },
+      process.env.JWT_SECRET || "default-secret",
+      jwtOptions
+    );
+    reply.send({ message: 'User logged in successfully', token });
   });
+
+  app.get('/users', async (request: FastifyRequest<{ Params: RouteParams }>, reply: FastifyReply) => {
+    const { id } = request.params
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id },
+      })
+      reply.send(user)
+    } catch (err) {
+      console.error(err)
+      reply.status(500).send({ message: 'Error fetching user.' })
+    }
+  })
+
+  // API routes
+  const { request } = require('graphql-request')
+
+  const query = `
+  query {
+    pokemons(first: 50) {
+      id
+      name
+      image
+    }
+  }
+`
+
+  app.get('/pokemons', async (req, res) => {
+    try {
+      const data = await request('https://graphql-pokemon2.vercel.app/', query)
+      res.send(data)
+    } catch (error) {
+      console.error(error)
+      res.send(error)
+    }
+  })
+
+}
+
+function getUserFromToken(authHeader: string) {
+  throw new Error("Function not implemented.");
 }
--- web/src/components/screens/home.tsx
+++ web/src/components/screens/home.tsx
@@ -1,5 +1,51 @@
-export function Home(){
-    return(
-        <div className="home"></div>
+import * as Avatar from '@radix-ui/react-avatar';
+import UserPhoto from '../../styles/assets/userphoto.png'
+import { useEffect, useState } from 'react';
+
+export function Home() {
+
+    const [username, setUsername] = useState('');
+
+    const userAvatar = () => (
+        <Avatar.Root >
+            <Avatar.Image src={UserPhoto} />
+            <Avatar.Fallback />
+        </Avatar.Root>
+        );
+
+        useEffect(() => {
+            async function getUsername() {
+                const response = await fetch(`http://localhost:2812/users?id=${userId}`, {
+                  headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+                });
+                const data = await response.json();
+                setUsername(data.username);
+              }
+              getUsername();
+          }, []);
+
+    return (
+        <div className="home">
+            <header className='headerHome'>
+                <div className="navbar">
+                    <img
+                        className="logo"
+                        src="https://fontmeme.com/permalink/230504/99e8e7a63ec54356a7e02a6e6a74d7d5.png"
+                        alt="pokemon-font"
+                        width={200}
+                    />
+                    <ul>
+                        <li>Home</li>
+                        <li>Pokelist</li>
+                        <li>Pokedex</li>
+                        <li>Battle Pass</li>
+                    </ul>
+                </div>
+                <div className='user'>
+                    <div>{username}</div>
+                    {userAvatar()}
+                </div>
+            </header>
+        </div>
     )
 }
