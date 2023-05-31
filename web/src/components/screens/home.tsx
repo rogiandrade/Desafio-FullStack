244daf9ae3160d1d24da6d019b90aa38d26d948d
Igor Andrade <rogiandradee@gmail.com>
2023-05-31 19:21:08 +0100

    home
    finish the home screen

--- web/src/components/screens/home.tsx
+++ web/src/components/screens/home.tsx
@@ -1,20 +1,30 @@
 import * as Avatar from '@radix-ui/react-avatar';
 import UserPhoto from '../../styles/assets/userphoto.png';
+import pokeballHome from '../../styles/assets/pokebolahome.png';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { api } from '../../lib/axios';
 import Cookies from 'universal-cookie';
 
+type Pokemon = {
+  number: string;
+  image: string;
+  name: string;
+  classification: string;
+  types: string[];
+  maxCP: number;
+};
 
 export function Home() {
-  
+
   const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [, setError] = useState(false);
+  const [data, setData] = useState<Pokemon[]>([])
 
   const userAvatar = () => (
     <Avatar.Root>
-      <Avatar.Image src={UserPhoto} />
+      <Avatar.Image src={UserPhoto} width={60} />
       <Avatar.Fallback />
     </Avatar.Root>
   );
@@ -60,8 +70,22 @@ export function Home() {
     navigate('/login');
   }*/
 
+  async function pokeList() {
+    try {
+      const response = await api.get('/pokemons');
+      const responseData = response.data;
+      setData(responseData.pokemons);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    pokeList();
+  }, []);
+
   return (
-    <div className="home">
+    <div id="home">
       <header className="headerHome">
         <div className="navbar">
           <img
@@ -71,18 +95,64 @@ export function Home() {
             width={200}
           />
           <ul>
-            <li>Home</li>
-            <li>Pokelist</li>
+            <li><a href='#home'>Home</a></li>
+            <li><a href='#pokeListCard'>Pokelist</a></li>
             <li>Pokedex</li>
             <li>Battle Pass</li>
           </ul>
         </div>
         <div className="user">
-          <div>{username}</div>
+          <div className='username'>{username}</div>
           {/* <button className='logout' onClick={handleLogout}>Logout</button> */}
           {userAvatar()}
         </div>
       </header>
+      <div className="content">
+        <div className="textcontent">
+          <h1 className='hometitle'>
+            Hello, welcome!
+            <br />
+            Let's explore the Pokemon universe with PokeDev!
+          </h1>
+          <p className='homesubtitle'>
+            Here you can check a list with all the pokemons in this beautiful world and you can even have your customized pokedex.
+          </p>
+          <button
+            className='pokeDexButton'
+          >
+            <a
+              className="pokeDexLink"
+              href={`http://127.0.0.1:5173/pokedex`}>Create a PokeDex!
+            </a>
+          </button>
+        </div>
+        <img className='pokeballHome' src={pokeballHome} alt="pokeball" width={500} />
+      </div>
+      <div id="pokeListCard">
+        <h1 className='contentTitle'>PokeList</h1>
+        <div className='pokeListContent'>
+          {data.map((pokemon) => (
+            <div
+              key={pokemon.number}
+            >
+              <p className='pokemonNumber'>{pokemon.number}</p>
+              <img
+                className='pokemonImg'
+                src={pokemon.image}
+                alt={pokemon.name}
+              />
+              <div
+                className='pokeList'
+              >
+                <p className='pokemonName'>{pokemon.name}</p>
+                <p className='pokemonClass'>{pokemon.classification}</p>
+                <p className='pokemonType'>{pokemon.types.join(', ')}</p>
+                <p className='pokemonCP'>Max CP:{pokemon.maxCP}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
